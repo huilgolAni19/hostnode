aafbd23bd18cf8fcf9be430e6235511c63b63460
Anirudh <anirudhhuilgol@Anirudhs-MacBook-Pro.local>
2018-06-25 11:06:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+const port = 3030
+
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, resp) => {
+    var data = {
+      users: getData()
+    }
+    resp.render('pages/indexpage', data);
+
+});
+
+app.get('/hello/:userName', (request, response) => {
+  //response.send("<h1>Hello World</h1>");
+  var usr = request.params.userName;
+  //console.log("User Name is: ", usr);
+
+
+  var data = {
+    user:usr
+  };
+
+  response.render('pages/firstPage', data);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  console.log(`Open http://localhost:${port}`);
+});
+
+
+var getData = () => {
+  var array = [];
+  var myObjecte1 = {};
+  var myObjecte2 = {};
+  var myObjecte3 = {};
+
+  myObjecte1["FirstName"] = "Anirudh";
+  myObjecte1["LastName"] = "Huilgol";
+  myObjecte1["Gender"] = "Male";
+  myObjecte1["Age"] = 24;
+  myObjecte1["City"] = "Hubli"
+  myObjecte1["Locality"] = "Vidyanagar"
+
+
+  myObjecte2["FirstName"] = "Pavan";
+  myObjecte2["LastName"] = "Pandurangi";
+  myObjecte2["Gender"] = "Male";
+  myObjecte2["Age"] = 27;
+  myObjecte2["City"] = "Mumbai"
+  myObjecte2["Locality"] = "Thane"
+
+  myObjecte3["FirstName"] = "Sohan";
+  myObjecte3["LastName"] = "Huilgol";
+  myObjecte3["Gender"] = "Male";
+  myObjecte3["Age"] = 26;
+  myObjecte3["City"] = "Bangalore"
+  myObjecte3["Locality"] = "Jayanagara"
+
+  array.push(myObjecte1);
+  array.push(myObjecte2);
+  array.push(myObjecte3);
+
+  return array;
+};
